ca5d05abd5e0d08134461e00b22826d9e7e30ae0
Paul Walker <starsinmypockets@gmail.com>
2020-09-15 11:08:03 -0400

    [tests][s] longer timeouts to cypress tests for ci

--- cypress/integration/resource-links.spec.js
+++ cypress/integration/resource-links.spec.js
@@ -14,9 +14,9 @@ beforeEach(() => {
 describe('Resource Page -- URL', () => {
   it('Submit Additional Metadata works', () => {
     cy.requiredMetadata();
-    cy.wait(1200);
+    cy.wait(5000);
     cy.additionalMetadata();
-    cy.wait(1200);
+    cy.wait(5000);
     cy.get('label[for=url]').click();
     cy.get('input[name=url]').type(chance.url());
     cy.get('input[name=name]').type(chance.word());
@@ -24,6 +24,7 @@ describe('Resource Page -- URL', () => {
     cy.get('select[name=mimetype]').select('Type 1');
     cy.get('input[name=format]').type(chance.word());
     cy.get('button[type=submit]').click();
+    cy.wait(5000);
     cy.visit('/dataset');
     cy.get('.dataset-list .dataset-item:first-child .dataset-content h3 > a').click();
     cy.get('.resource-list').find('.resource-item').should('have.length', 1);
