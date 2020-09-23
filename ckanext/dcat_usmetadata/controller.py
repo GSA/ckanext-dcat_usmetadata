ac17f66abfb9b4c91bf00b9bad5f9821ca54e8f8
anuveyatsu <anuar.ustayev@gmail.com>
2020-09-23 14:56:32 +0600

    [new metadata][xs]: removed old routes as not needed anymore.

--- ckanext/dcat_usmetadata/controller.py
+++
@@ -1,6 +0,0 @@
-from ckan.lib import base
-from ckan.controllers.package import PackageController as CorePackageController
-
-class CustomPageController(CorePackageController):
-    def load_metadata_form(self):
-        return base.render('new-metadata.html')
--- ckanext/dcat_usmetadata/plugin.py
+++ ckanext/dcat_usmetadata/plugin.py
@@ -4,21 +4,8 @@ import ckan.plugins.toolkit as toolkit
 
 class Dcat_UsmetadataPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
-    plugins.implements(plugins.IRoutes)
 
     # IConfigurer
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
-
-    # IRoutes
-    def before_map(self, map):
-        controller = 'ckanext.dcat_usmetadata.controller:CustomPageController'
-
-        map.connect('/dataset/new-metadata',
-                    controller=controller,
-                    action='load_metadata_form')
-        return map
-
-    def after_map(self, map):
-        return map
