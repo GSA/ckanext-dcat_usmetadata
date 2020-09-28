37f4c0393d635e74836c1933c643dd42a2e1bf58
Paul Walker <starsinmypockets@gmail.com>
2020-09-28 18:34:51 -0400

    Feature/resource types 2 (#66)
    * [resources][m] Add resource mimetypes
    
    * [build]
    
    * [requirements] Update build requirements
    
    * [requirements] Update requirements
    
    * [tests][s] Fix mimetype for resource link test

--- cypress/integration/resource-links.spec.js
+++ cypress/integration/resource-links.spec.js
@@ -21,7 +21,7 @@ describe('Resource Page -- URL', () => {
     cy.get('input[name=resource\\.url]').type(chance.url());
     cy.get('input[name=resource\\.name]').type(chance.word());
     cy.get('textarea[name=resource\\.description]').type(chance.sentence({ words: 10 }));
-    cy.get('select[name=resource\\.mimetype]').select('Type 1');
+    cy.get('select[name=resource\\.mimetype]').select('DOC -- Word Document');
     cy.get('input[name=resource\\.format]').type(chance.word());
     cy.get('button[type=button]').contains('Finish and publish').click();
     cy.wait(5000);
