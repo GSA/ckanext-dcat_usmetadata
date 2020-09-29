3b43cd85eef396c117e6814f701183cf3cd46bff
anuveyatsu <anuar.ustayev@gmail.com>
2020-09-29 14:19:03 +0600

    [save draft][xs]: added '0' for hours and minutes so that we always display 2 digit values.

--- metadata-app/src/components/MetadataForm/index.js
+++ metadata-app/src/components/MetadataForm/index.js
@@ -27,7 +27,7 @@ const formatErrors = (errors) =>
   });
 
 const formatDate = (date) => {
-  return `${[date.getHours(), date.getMinutes()].join(':')}, ${[
+  return `${[`0${date.getHours()}`.slice(-2), `0${date.getMinutes()}`.slice(-2)].join(':')}, ${[
     `0${date.getMonth() + 1}`.slice(-2),
     `0${date.getDate()}`.slice(-2),
     date.getFullYear().toString().slice(-2),
