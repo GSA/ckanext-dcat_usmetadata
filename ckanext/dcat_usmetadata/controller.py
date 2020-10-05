77ab57f3ecf3941fc492a7c52774da16496db7af
Anuar Ustayev <anuar.ustayev@gmail.com>
2020-10-05 21:36:48 +0600

    [controller][xs]: bug fixes - import missing variables.

--- ckanext/dcat_usmetadata/controller.py
+++ ckanext/dcat_usmetadata/controller.py
@@ -1,10 +1,11 @@
 from ckan.lib import base
 import ckan.logic as logic
 import ckan.model as model
-from ckan.common import request, c
+from ckan.common import request, c, _
 from ckan.controllers.package import PackageController as CorePackageController
 
 check_access = logic.check_access
+NotAuthorized = logic.NotAuthorized
 
 class MetadataController(CorePackageController):
     def load_metadata_form(self):
@@ -14,5 +15,5 @@ class MetadataController(CorePackageController):
         try:
             check_access('package_create', context)
         except NotAuthorized:
-            abort(403, _('Unauthorized to create a package'))
+            base.abort(403, _('Unauthorized to create a package'))
         return base.render('new-metadata.html')
