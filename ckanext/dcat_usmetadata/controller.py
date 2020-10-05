8d755eca27b73b6e7933b23f88ce2556f1168a58
Anuar Ustayev <anuveyatsu@Anuars-MacBook-Pro.local>
2020-10-05 13:50:05 +0600

    [access][xs]: restrict access to metadata form from the controller.

--- ckanext/dcat_usmetadata/controller.py
+++ ckanext/dcat_usmetadata/controller.py
@@ -1,6 +1,18 @@
 from ckan.lib import base
+import ckan.logic as logic
+import ckan.model as model
+from ckan.common import request, c
 from ckan.controllers.package import PackageController as CorePackageController
+
+check_access = logic.check_access
 
 class MetadataController(CorePackageController):
     def load_metadata_form(self):
+        context = {'model': model, 'session': model.Session,
+                   'user': c.user, 'auth_user_obj': c.userobj,
+                   'save': 'save' in request.params}
+        try:
+            check_access('package_create', context)
+        except NotAuthorized:
+            abort(403, _('Unauthorized to create a package'))
         return base.render('new-metadata.html')
