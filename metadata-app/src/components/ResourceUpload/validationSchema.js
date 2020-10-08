89092cd6a9b6df323fdc58a5767a3e3b762a0bb7
Anuar Ustayev <anuar.ustayev@gmail.com>
2020-10-08 22:55:00 +0600

    [bug fix][xs]: bug fix in yup schema for resource object.

--- metadata-app/src/components/ResourceUpload/validationSchema.js
+++ metadata-app/src/components/ResourceUpload/validationSchema.js
@@ -1,12 +1,12 @@
 import * as yup from 'yup';
 
 export default yup.object().shape({
-  resource: {
+  resource: yup.object().shape({
     name: yup.string(),
     description: yup.string(),
     mimetype: yup.string(),
     format: yup.string(),
     url: yup.string().url(),
     upload: yup.mixed(),
-  },
+  }),
 });
