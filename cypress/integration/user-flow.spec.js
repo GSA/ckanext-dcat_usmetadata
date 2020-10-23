7cdb153685f6fabd79364b19d020369c038d42e9
Anuar Ustayev <anuar.ustayev@gmail.com>
2020-10-23 19:04:48 +0600

    [tests][s]: make sure an editor can select an organization when creating a new dataset.

--- cypress/integration/user-flow.spec.js
+++ cypress/integration/user-flow.spec.js
@@ -20,3 +20,29 @@ describe('Access to the new metadata app', () => {
     cy.get('.navsec').should('not.exist');
   });
 });
+
+describe('List of organizations on new metadata form', () => {
+  before(() => {
+    cy.createUser('editor');
+    cy.login();
+    cy.visit('/organization/member_new/test-123');
+    cy.get('input[name=username]').type('editor', { force: true });
+    cy.get('select[name=role]').select('Editor', { force: true });
+    cy.get('button[name=submit]').click({ force: true });
+    cy.wait(2000);
+  });
+
+  it('Displays expected organizations for the editor role', () => {
+    cy.logout();
+    cy.visit('/user/login');
+    cy.get('input[name=login]').type('editor');
+    cy.get('input[name=password]').type('test1234');
+    cy.get('.form-actions > .btn').click({ force: true });
+    cy.wait(2000);
+    cy.visit('/dataset');
+    cy.get('.page_primary_action > .btn').click();
+    cy.get('.navsec').contains('Required Metadata');
+    cy.get('select[name=owner_org]').select('test-123')
+      .should('contain.text', 'test-123');
+  })
+})
