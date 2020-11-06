41aa0a7f01a516e4554fd0cc0d378a00f3c34359
Anuar Ustayev <anuar.ustayev@gmail.com>
2020-11-06 17:40:33 +0600

    [redirect][xs]: if user is anonymous, redirect to login page.

--- ckanext/dcat_usmetadata/controller.py
+++ ckanext/dcat_usmetadata/controller.py
@@ -1,4 +1,5 @@
 from ckan.lib import base
+import ckan.lib.helpers as h
 import ckan.logic as logic
 import ckan.model as model
 from ckan.common import request, c, _
@@ -9,6 +10,16 @@ NotAuthorized = logic.NotAuthorized
 
 class MetadataController(CorePackageController):
     def load_metadata_form(self):
+        if not c.user:
+            err = _('Unauthorized to create a package')
+            h.flash_error(err)
+            came_from = h.url_for(
+                controller = 'ckanext.dcat_usmetadata.controller:MetadataController',
+                action='load_metadata_form'
+            )
+            h.redirect_to(controller='user',
+                          action='login', came_from=came_from)
+
         context = {'model': model, 'session': model.Session,
                    'user': c.user, 'auth_user_obj': c.userobj,
                    'save': 'save' in request.params}
