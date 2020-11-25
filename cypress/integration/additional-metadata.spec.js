2b170dbe8e1ff65a562620abc5c615fcec996c51
Anuar Ustayev <anuar.ustayev@gmail.com>
2020-11-25 15:08:30 +0600

    [tests][s]: improved tests using new 'intercept' feature.

--- cypress/integration/additional-metadata.spec.js
+++ cypress/integration/additional-metadata.spec.js
@@ -14,34 +14,32 @@ beforeEach(() => {
 
 describe('Additional Metadata Page', () => {
   it('Validates and submits Additional Metadata succesfully', () => {
-    cy.requiredMetadata().then(() => {
-      cy.get('input[name=data_dictionary]').type('www.invalid.url');
-      cy.get('input[name=homepage_url]').type('www.invalid.url');
-      cy.get('button[type=button]').contains('Save and Continue').click();
-      cy.get('h3.usa-alert__heading').contains('This form contains invalid entries');
-      cy.get('.usa-alert__text').contains('data_dictionary must be a valid URL');
-      cy.get('.usa-alert__text').contains('homepage_url must be a valid URL');
-      cy.additionalMetadata();
-      cy.get('button[type=button]')
-        .contains('Save and Continue')
-        .click()
-        .then(() => {
-          cy.contains(
-            'You can add the URL of the dataset where it is available on the agency website.'
-          );
-        });
-    });
+    cy.requiredMetadata();
+    cy.get('input[name=data_dictionary]').type('www.invalid.url');
+    cy.get('input[name=homepage_url]').type('www.invalid.url');
+    cy.get('button[type=button]').contains('Save and Continue').click();
+    cy.get('h3.usa-alert__heading').contains('This form contains invalid entries');
+    cy.get('.usa-alert__text').contains('data_dictionary must be a valid URL');
+    cy.get('.usa-alert__text').contains('homepage_url must be a valid URL');
+    cy.additionalMetadata();
+    cy.get('button[type=button]')
+      .contains('Save and Continue')
+      .click()
+      .then(() => {
+        cy.contains(
+          'You can add the URL of the dataset where it is available on the agency website.'
+        );
+      });
   });
 
   it('Goes back to previous page', () => {
-    cy.requiredMetadata().then(() => {
-      cy.get('button[type=button]')
-        .contains('Back to previous page')
-        .click()
-        .then(() => {
-          cy.contains('The following fields are required metadata');
-        });
-    });
+    cy.requiredMetadata();
+    cy.get('button[type=button]')
+      .contains('Back to previous page')
+      .click()
+      .then(() => {
+        cy.contains('The following fields are required metadata');
+      });
   });
 });
 
