61dfb13f047e4ad0bce90c5e5bf2bbdcb1614269
Chris MacDermaid <christopher.macdermaid@gsa.gov>
2020-12-03 12:39:30 -0700

    Change static/public dir to public/assets becase of CKAN bug

--- react-scripts-config/webpack.config.js
+++ react-scripts-config/webpack.config.js
@@ -79,7 +79,7 @@ module.exports = function (webpackEnv) {
       isEnvDevelopment && require.resolve('style-loader'),
       isEnvProduction && {
         loader: MiniCssExtractPlugin.loader,
-        // css is located in `static/css`, use '../../' to locate index.html folder
+        // css is located in `assets/css`, use '../../' to locate index.html folder
         // in production `paths.publicUrlOrPath` can be a relative path
         options: paths.publicUrlOrPath.startsWith('.') ? { publicPath: '../../' } : {},
       },
@@ -168,13 +168,13 @@ module.exports = function (webpackEnv) {
       pathinfo: isEnvDevelopment,
       // There will be one main bundle, and one file per asynchronous chunk.
       // In development, it does not produce real files.
-      filename: isEnvProduction ? 'stat/js/[name].js' : isEnvDevelopment && 'stat/js/bundle.js',
+      filename: isEnvProduction ? 'assets/js/[name].js' : isEnvDevelopment && 'assets/js/bundle.js',
       // TODO: remove this when upgrading to webpack 5
       futureEmitAssets: true,
       // There are also additional JS chunk files if you use code splitting.
       chunkFilename: isEnvProduction
-        ? 'stat/js/[name].chunk.js'
-        : isEnvDevelopment && 'stat/js/[name].chunk.js',
+        ? 'assets/js/[name].chunk.js'
+        : isEnvDevelopment && 'assets/js/[name].chunk.js',
       // webpack uses `publicPath` to determine where the app is being served from.
       // It requires a trailing slash, or the file assets will get an incorrect path.
       // We inferred the "public path" (such as / or /my-project) from homepage.
@@ -351,7 +351,7 @@ module.exports = function (webpackEnv) {
               loader: require.resolve('url-loader'),
               options: {
                 limit: imageInlineSizeLimit,
-                name: 'stat/media/[name].[hash:8].[ext]',
+                name: 'assets/media/[name].[hash:8].[ext]',
               },
             },
             // Process application JS with Babel.
@@ -487,7 +487,7 @@ module.exports = function (webpackEnv) {
               // by webpacks internal loaders.
               exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
               options: {
-                name: 'stat/media/[name].[hash:8].[ext]',
+                name: 'assets/media/[name].[hash:8].[ext]',
               },
             },
             // ** STOP ** Are you adding a new loader?
@@ -560,8 +560,8 @@ module.exports = function (webpackEnv) {
         new MiniCssExtractPlugin({
           // Options similar to the same options in webpackOptions.output
           // both options are optional
-          filename: 'stat/css/[name].css',
-          chunkFilename: 'stat/css/[name].chunk.css',
+          filename: 'assets/css/[name].css',
+          chunkFilename: 'assets/css/[name].chunk.css',
         }),
       // Generate an asset manifest file with the following content:
       // - "files" key: Mapping of all asset filenames to their corresponding
