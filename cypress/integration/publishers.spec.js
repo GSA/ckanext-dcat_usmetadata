6952ab3d2ede38331a89aa7b58f8262b9760edfb
Anuar Ustayev <anuar.ustayev@gmail.com>
2021-02-05 13:38:39 +0600

    [tests][s]: check if publishers are loaded as expected.

---
+++ cypress/integration/publishers.spec.js
@@ -0,0 +1,15 @@
+describe('Publishers linked to CKAN org', () => {
+  it('Has publishers extra in CKAN org metadata', () => {
+    cy.login();
+    cy.request('GET', '/api/3/action/organization_show?id=test-organization').then((response) => {
+      expect(response.status).to.eq(200);
+      const publisherExtra = response.body.result.extras.find((x) => x.key === 'publisher');
+      assert.isObject(publisherExtra, 'extra is found');
+      assert.isString(publisherExtra.value, 'its value is string');
+      const publishers = JSON.parse(publisherExtra.value);
+      expect(publishers.length).to.eq(6);
+      expect(publishers[0].length).to.eq(2);
+      expect(publishers[5].length).to.eq(7);
+    });
+  });
+});
