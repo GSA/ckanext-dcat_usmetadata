d3a253ea7f0f1d169a90e230441d8552c56279b5
robert-bryson <robert.bryson@gsa.gov>
2023-07-17 16:39:41 +0000

    lint with black/isort

--- ckanext/dcat_usmetadata/cli.py
+++ ckanext/dcat_usmetadata/cli.py
@@ -2,24 +2,22 @@ import csv
 import json
 
 import ckan.plugins as p
-from ckan import model
-
 import click
+from ckan import model
 
 
 def get_commands():
     return [dcat_usmetadata]
 
 
-@click.group(u"dcat-usmetadata")
+@click.group("dcat-usmetadata")
 def dcat_usmetadata():
-    """Does DCAT-US metadata setup.
-    """
+    """Does DCAT-US metadata setup."""
     pass
 
 
-@dcat_usmetadata.command(u"import-publishers")
-@click.argument(u"path_to_file")
+@dcat_usmetadata.command("import-publishers")
+@click.argument("path_to_file")
 def import_publishers(path_to_file):
     with open(path_to_file) as csvfile:
         reader = csv.reader(csvfile)
@@ -47,22 +45,27 @@ def get_orgs_to_process(rows):
 def update_orgs_with_publishers(org, publishers_tree):
     # Update org metadata with the publishers data:
     try:
-        user = p.toolkit.get_action('get_site_user')({'model': model, 'ignore_auth': True}, {})
-        org_metadata = p.toolkit.get_action(
-            'organization_show')({"user": user['name'], "ignore_auth": True}, {'id': org})
-        org_extras = org_metadata.get('extras', [])
+        user = p.toolkit.get_action("get_site_user")(
+            {"model": model, "ignore_auth": True}, {}
+        )
+        org_metadata = p.toolkit.get_action("organization_show")(
+            {"user": user["name"], "ignore_auth": True}, {"id": org}
+        )
+        org_extras = org_metadata.get("extras", [])
         index_of_publisher_extra = next(
-            (i for i, item in enumerate(org_extras)
-                if item['key'] == 'publisher'), None)
+            (i for i, item in enumerate(org_extras) if item["key"] == "publisher"), None
+        )
         if index_of_publisher_extra:
-            org_extras[index_of_publisher_extra]['value'] = json.dumps(
-                publishers_tree)
+            org_extras[index_of_publisher_extra]["value"] = json.dumps(publishers_tree)
         else:
             org_extras.append(
-                {'key': 'publisher', 'value': json.dumps(publishers_tree)})
+                {"key": "publisher", "value": json.dumps(publishers_tree)}
+            )
 
-        p.toolkit.get_action('organization_patch')(
-            {"user": user['name'], "ignore_auth": True}, {'id': org, 'extras': org_extras})
+        p.toolkit.get_action("organization_patch")(
+            {"user": user["name"], "ignore_auth": True},
+            {"id": org, "extras": org_extras},
+        )
         print("Updated publishers for '{}'".format(org))
     except Exception as e:
         print(e)
--- ckanext/dcat_usmetadata/plugin.py
+++ ckanext/dcat_usmetadata/plugin.py
@@ -1,8 +1,10 @@
-from ckan.common import c
+from logging import getLogger
+
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from ckan.common import c
+
 import ckanext.dcat_usmetadata.cli as cli
-from logging import getLogger
 
 from . import blueprint
 from . import db_utils as utils
@@ -25,16 +27,17 @@ class Dcat_UsmetadataPlugin(plugins.SingletonPlugin):
 
     # IConfigurer
     def update_config(self, config):
-        plugins.toolkit.add_template_directory(config, 'templates')
-        plugins.toolkit.add_resource('fanstatic', 'dcat_usmetadata')
-        plugins.toolkit.add_public_directory(config, 'public')
+        plugins.toolkit.add_template_directory(config, "templates")
+        plugins.toolkit.add_resource("fanstatic", "dcat_usmetadata")
+        plugins.toolkit.add_public_directory(config, "public")
 
     # IActions
     def get_actions(self):
         def parent_dataset_options(context, data_dict):
             return utils.get_parent_organizations(c)
+
         return {
-            'parent_dataset_options': parent_dataset_options,
+            "parent_dataset_options": parent_dataset_options,
         }
 
     def get_blueprint(self):
