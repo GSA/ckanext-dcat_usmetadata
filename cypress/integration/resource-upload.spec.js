d65b762f6674d1a45ac11dfec02bf546233aad1f
Fuhu Xia <fxia@reisystems.com>
2026-08-10 15:52:07 -0400

    fix flaky resource-upload.spec.js

--- cypress/integration/resource-upload.spec.js
+++ cypress/integration/resource-upload.spec.js
@@ -301,9 +301,8 @@ describe('Editing resources', () => {
 
   it('Works when editing a resource in "edit" mode', () => {
     cy.requiredMetadata(name);
-    cy.intercept('/api/3/action/package_update').as('packageUpdate');
     cy.get('button[type=button]').contains('Save and Continue').click();
-    cy.wait('@packageUpdate');
+    cy.contains('Save and add another resource');
 
     // Create 2 resources: 1 with link and 1 upload:
     cy.intercept('/api/3/action/resource_create').as('resourceCreate');
